6c8633334972d1a98fb45638af79b7bd0b6a20b9
MaxiMy <maximy.pro@gmail.com>
2022-04-15 08:12:55 +0300

    create register form

--- atlas/authapp/forms.py
+++ atlas/authapp/forms.py
@@ -4,18 +4,20 @@ from django import forms
 
 
 class RegisterUserForm(UserCreationForm):
+    password1 = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control form-control-user', 'type': 'password', 'name': 'password',
+               'id': 'exampleInputPassword', 'placeholder': 'Password'}),
+        label='')
+    password2 = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control form-control-user', 'type': 'password', 'name': 'password',
+               'id': 'exampleRepeatPassword', 'placeholder': 'Repeat Password'}),
+        label='')
     class Meta:
         model = User
         fields = ('username','password1','password2')
         widgets = {
-            'username': forms.EmailInput(attrs={'class': 'form-control form-control-user',
+            'username': forms.TextInput(attrs={'class': 'form-control form-control-user',
                                                'id':'exampleInputEmail',
-                                               'placeholder':'username'
+                                               'placeholder':'Username'
                                                }),
         }
-        password1 = forms.CharField(widget=forms.PasswordInput(
-            attrs={'class': 'form-control form-control-user', 'type': 'password', 'name': 'password','id':'exampleInputPassword', 'placeholder': 'Password'}),
-            label='')
-        password2 = forms.CharField(widget=forms.PasswordInput(
-            attrs={'class': 'form-control form-control-user', 'type': 'password', 'name': 'password','id':'exampleRepeatPassword', 'placeholder': 'Repeat Password'}),
-            label='')
--- atlas/authapp/views.py
+++ atlas/authapp/views.py
@@ -3,9 +3,15 @@ from django.views.generic import CreateView
 from django.urls import reverse, reverse_lazy
 
 from .forms import RegisterUserForm
+from .utils import DataMixin
 
 
-class RegisterUser(CreateView):
+class RegisterUser(DataMixin,CreateView):
     form_class = RegisterUserForm
     template_name = 'authapp/register.html'
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('authapp:login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
